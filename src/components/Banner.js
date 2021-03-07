fdc75f4e3904b4d2841371141aef4ef481ed89dc
vivek singh <vikky30singh@gmail.com>
2021-03-07 13:32:23 +0530

    modified Banner

---
+++ src/components/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <div className="section center teal-text text-lighten-1">
+            <a id="homelink" href="/">
+                <span className="flow-text blue-text text-darken-2" id="banner">Chat App</span>
+                <span id="logo"><i className="material-icons blue-text text-darken-2">forum</i></span>
+            </a>
+            <span className="right green-text text-darken-4" id="active-users"></span>
+        </div>
+    )
+}
+
+export default Banner
