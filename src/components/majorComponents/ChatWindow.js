d05e16c8747c8ee7c122060cd5960b7988392e6e
vivek singh <vikky30singh@gmail.com>
2021-03-09 00:47:28 +0530

    listened for chat event and displayed messages on chat window

--- src/components/majorComponents/ChatWindow.js
+++ src/components/majorComponents/ChatWindow.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react'
+import React,{ useState, useEffect } from 'react'
+import { socket } from '../../clientStuff/chat'
+import Message from '../Message'
 
-class ChatWindow extends Component {
-    render() {
-        return (
-            <div id="chat-window">
-                <div id="display"></div>
-                <div id="status-bar" className="teal-text text-darken-2"></div>
+const ChatWindow = () => {
+
+    const [ messages, setMessages ] = useState([])
+
+    useEffect(() => {
+        socket.on('chat',({ message }) => {
+            setMessages(messages => [ ...messages, message ])
+        })
+    }, [])
+    
+    return (
+        <div id="chat-window">
+            <div id="display">
+                <Message messages={messages} />
             </div>
-        )
-    }
+            <div id="status-bar" className="teal-text text-darken-2"></div>
+        </div>
+    )
 }
 
 export default ChatWindow
