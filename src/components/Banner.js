8a616a427da4d5b41ffe131cadb0d7142bfe574c
vivek singh <vikky30singh@gmail.com>
2021-03-08 16:18:28 +0530

    minor change

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Banner() {
     return (
         <div className="section center teal-text text-lighten-1">
-            <a id="homelink" href="/">
+            <a id="homelink" href="/" target="_blank">
                 <span className="flow-text blue-text text-darken-2" id="banner">Chat App</span>
                 <span id="logo"><i className="material-icons blue-text text-darken-2">forum</i></span>
             </a>
