318a36fe3affdd04eeeb9310d7b9782778421898
vivek singh <vikky30singh@gmail.com>
2021-03-09 00:46:10 +0530

    made user field to auto focus and handled messages by emitting chat event

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import { socket } from '../clientStuff/chat';
+import { customAlert } from '../clientStuff/helperFunctions'
 
-function Form() {
+const Form = () => {
 
    const [ username, setUsername ] = useState('');
    const [ message, setMessage ] = useState('');
+   const [ isDisabled, setIsDisabled ] = useState(false);
+   const userRef = useRef(null)
+   const messageRef = useRef(null)
+
+    useEffect(() => {
+       userRef.current.focus() // Make focus on username input field
+    },[])
 
     const handleSubmit = (e) => { 
         e.preventDefault();
-        // alert('username '+username+' message '+message)
         if(message.trim() === '' || username.trim() === ''){
-            alert('empty field are not allowed');
+            customAlert('Empty fields are not allowed')
+            return;
         }
-        setUsername('')
-        setMessage('')
-        // else{
-        //     user.disabled = true;
+
+        if(!isDisabled){
+            setIsDisabled(true);
+        }
+
+        socket.emit('chat',{
+            user: username,
+            message
+        })
+
+        // clear input field for fresh message
+        setMessage('') 
     
         //     display.innerHTML+= `
         //         <div class="chat-msg myright">
@@ -22,7 +39,6 @@ function Form() {
         //         </div>`;
     
         //     display.scrollTop = display.scrollHeight;
-        // }
     
     }
 
@@ -39,12 +55,12 @@ function Form() {
         <form className="center section" id="chat-box" onSubmit={handleSubmit} >
             <div className="input-field">
                 <i className="material-icons prefix blue-text text-darken-2">person</i>
-                <input id="user" type="text" onChange={handleChange} value={username} autoComplete="off" />
+                <input id="user" type="text" ref={userRef} onChange={handleChange} value={username} autoComplete="off" disabled={isDisabled} />
                 <label htmlFor="user">Your Name</label>
             </div>
             <div className="input-field">
                 <i className="material-icons prefix blue-text text-darken-2">chat</i>
-                <input id="message" type="text" onChange={handleChange} value={message} autoComplete="off" />
+                <input id="message" type="text" ref={messageRef} onChange={handleChange} value={message} autoComplete="off" />
                 <label htmlFor="message">Your Message</label>
             </div>
             <button type="submit" className="btn blue darken-2 z-depth-2" id="send-btn">
