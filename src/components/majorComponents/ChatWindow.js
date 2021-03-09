c0249d5b74c1b078d53f85a955772587ca214229
vivek singh <vikky30singh@gmail.com>
2021-03-09 16:29:12 +0530

    handled major event of messaging and displayed messages to users, also enabled auto scroll to bottom

--- src/components/majorComponents/ChatWindow.js
+++ src/components/majorComponents/ChatWindow.js
@@ -1,23 +1,50 @@
-import React,{ useState, useEffect } from 'react'
-import { socket } from '../../clientStuff/chat'
-import Message from '../Message'
+import React,{ useState, useEffect, useRef } from 'react'
+import { socket } from '../../clientStuff/Chat'
+import Message from '../../clientStuff/Message'
 
 const ChatWindow = () => {
 
-    const [ messages, setMessages ] = useState([])
+    const [ data, setData ] = useState([])
+    const [ user, setUser ] = useState('')
+    const [ CSS, setCSS ] = useState({
+        backgroundColor: '#c2c2c2',
+        padding: '8px 20px'
+    })
+
+    const bottomScrollRef = useRef(null)
 
     useEffect(() => {
-        socket.on('chat',({ message }) => {
-            setMessages(messages => [ ...messages, message ])
+        socket.on('chat',(newData) => {
+            setUser('')
+            setData(data => [ ...data, newData ])
+            setCSS({
+                ...CSS,
+                display: 'none',
+            })
+        })
+
+        socket.on('status',(user) => {
+            setCSS({
+                ...CSS,
+                display: 'block',
+            })
+            setUser(user)
         })
     }, [])
+
+    useEffect(() => {
+        bottomScrollRef.current?.scrollIntoView({ behaviour: "smooth" })
+    }, [data])
     
     return (
         <div id="chat-window">
             <div id="display">
-                <Message messages={messages} />
+                <Message data={data} />
+                <div ref={bottomScrollRef}></div>
             </div>
-            <div id="status-bar" className="teal-text text-darken-2"></div>
+            <div id="status-bar" className="teal-text text-darken-2" style={CSS} >
+                <em>{user} is typing ...</em>
+            </div>
         </div>
     )
 }
