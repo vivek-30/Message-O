1f801eb368797f31f5bd5ab72c10bfd8a6c88ed0
vivek singh <vikky30singh@gmail.com>
2021-03-09 16:25:26 +0530

    handled status and active-user events

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import { socket } from '../clientStuff/Chat'
+import { customAlert } from '../clientStuff/helperFunctions'
 
-function Banner() {
+const Banner = () => {
+
+    const [ activeUsers, setActiveUsers ] = useState(0)
+
+    useEffect(() => {
+        socket.on('total-users',(totalUsers) => {
+            setActiveUsers(totalUsers)
+        })
+
+        socket.on('leave',({ totalUsers, user }) => {
+            setActiveUsers(totalUsers)
+            if(user){
+                customAlert(`${user} Left The Chat`)
+            }
+        })
+    },[])
+
     return (
         <div className="section center teal-text text-lighten-1">
             <a id="homelink" href="/" target="_blank">
                 <span className="flow-text blue-text text-darken-2" id="banner">Chat App</span>
                 <span id="logo"><i className="material-icons blue-text text-darken-2">forum</i></span>
             </a>
-            <span className="right green-text text-darken-4" id="active-users"></span>
+            <span className="right green-text text-darken-4" id="active-users">{activeUsers} Active</span>
         </div>
     )
 }
