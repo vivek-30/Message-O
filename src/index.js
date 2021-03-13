3179b5eb15d2d0baec974642e20fd5ca18297ecf
vivek singh <vikky30singh@gmail.com>
2021-03-13 21:20:28 +0530

    removed js file of materialize css and imported it in index.js instead

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
 import './index.css'
 
 ReactDOM.render(
