b8a1073407ea5df291b316479e7c1b3f6337da6f
vivek singh <vikky30singh@gmail.com>
2021-03-17 20:04:08 +0530

    handled new route in backend

--- server/router.js
+++ server/router.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res) => {
-    res.status(200).send('server is successfully running....')
+    res.status(200).send({
+        response: 'Server is successfully running ....',
+        message: 'Please authenticate yourself to use this service.'
+    })
+});
+
+router.get('/chat',(req,res) => {
+    res.status(200).send({
+        response: 'Server is successfully running....',
+        message: 'Happy Chating.'
+    })
 });
 
 module.exports = router;
