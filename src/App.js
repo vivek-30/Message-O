10d7d298046e8b0815aaa9ce5e655d9662809c3b
vivek singh <vikky30singh@gmail.com>
2021-03-18 18:20:50 +0530

    handled all new routes on client side

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import Banner from './components/Banner'
 import Form from './components/Form'
 import Toolbar from './components/Toolbar'
 import ChatWindow from './components/majorComponents/ChatWindow'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import SignUp from './components/authentication/SignUp'
+import NavBar from './components/authentication/NavBar'
+import SignIn from './components/authentication/SignIn'
 
 const App = () => {
   return (
     <div className="container" id="chat-app">
-      <Banner />
-      <ChatWindow />
-      <Toolbar />
-      <Form />
+
+      <Router>
+      <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <SignUp />
+          </Route>
+            <Route path="/SignUp">
+              <SignUp />
+            </Route>
+            <Route path="/SignIn">
+              <SignIn />
+            <Link to="/chat" className="center">go</Link>
+          </Route>
+          <Route path="/chat">
+            <Banner />
+            <ChatWindow />
+            <Toolbar />
+            <Form />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
