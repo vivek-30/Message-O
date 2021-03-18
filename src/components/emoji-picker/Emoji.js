4d7f96404897237a01af17dc50ed82fd52385baa
vivek singh <vikky30singh@gmail.com>
2021-03-18 18:16:27 +0530

    changed emoji picker styles

---
+++ src/components/emoji-picker/Emoji.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { Picker } from 'emoji-mart';
+import { CustomEmotes } from './CustomEmotes'
+import { displayEmoji } from '../Form'
+import './emojiStyle.css'
+
+const pickerStyle = { 
+    top: '8rem',
+    right: '3rem',
+    width: '15rem',  
+    position: 'absolute',
+    backgroundColor: '#f1f1f1',
+    boxShadow: '2px 2px 8px 1px rgba(0,0,1,0.3)'
+}
+
+const Emoji = () => {
+
+    const [ picker, setPicker ] = useState(true)
+
+    return (
+        <span className="reactions">
+            <Picker
+            showPreview={false} 
+            showSkinTones={false}
+            onSelect={(emoji) => {
+                setPicker(!picker)
+                displayEmoji(emoji)
+            }}
+            emojiSize={30} 
+            style={{ display: picker ? 'block' : 'none', ...pickerStyle }}
+            include={['custom']}
+            custom={CustomEmotes} 
+            />
+        </span>
+    )
+}
+
+export default Emoji 
