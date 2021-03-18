90ee77ee133cc22f3d4d46d4e4f12014c3db5c72
vivek singh <vikky30singh@gmail.com>
2021-03-18 18:17:52 +0530

    created Navbar for authentication pages

---
+++ src/components/authentication/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div className="nav-wrapper">
+                <NavLink to="/" className="brand-logo">Chat App</NavLink>
+                <ul className="right">
+                    <li><Link to="/SignIn">Sign In</Link></li>
+                    <li><Link to="/SignUp">Sign Up</Link></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
