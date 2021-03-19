c15848d1c06fc5190c323a6a195da0fb6ecdf1da
vivek singh <vikky30singh@gmail.com>
2021-03-20 00:15:12 +0530

    used TakeInput inside SignIn.js

--- src/components/authentication/SignIn.js
+++ src/components/authentication/SignIn.js
@@ -1,28 +1,48 @@
-import React from 'react'
-import './auth.css'
+import React, { useState } from 'react'
+import TakeInput from '../main/TakeInput'
+import './authenticate.css'
 
 const SignIn = () => {
 
-    var User = ''
+    const [ user, setUser ] = useState('')
+    const [ password, setPassword ] = useState('')
 
     const handleChange = (e) => {
-        let value = e.target.value
-        if(e.target.name === 'username'){
-            User = value
-        }
+        const { id, value } = e.target
+        id === 'user-name' ? setUser(value) : id ===  'password' ? setPassword(value) : null
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(`user = ${user} password = ${password}`)
+        setUser('')
+        setPassword('')
+    }
+
     return (
-        <form className="auth" onSubmit={(e) => e.preventDefault()}>
-            <div className="input-field">
-                <i className="material-icons prefix blue-text text-darken-2">person</i>
-                <input id="username" type="text" autoComplete="off" onChange={handleChange} value={User} />
-                <label htmlFor="username">Enter Your Username</label>
-            </div>
-            <div className="input-field">
-                <i className="material-icons prefix blue-text text-darken-2">lock</i>
-                <input id="password" type="password" autoComplete="off" />
-                <label htmlFor="password">Enter Your Password</label>
-            </div>
+        <form className="authenticate" onSubmit={handleSubmit}>
+            <TakeInput 
+                iconName="person" 
+                labelText="Enter Your Username"
+                options={{
+                    id: "user-name", 
+                    type: 'text',
+                    handleChange,
+                    value: user
+                }}
+            />
+
+            <TakeInput 
+                iconName="lock"  
+                labelText="Enter Your Password"
+                options={{
+                    id: "password", 
+                    type: 'password',
+                    handleChange,
+                    value: password
+                }}
+            />
+
             <button type="submit" className="btn blue darken-2 z-depth-2">
                 <span>Sign In</span>
                 <i className="material-icons right">login</i>
