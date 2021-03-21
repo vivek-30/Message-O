31fe20a6497c1dd89aad8a38c20fb129e7c54aab
vivek singh <vikky30singh@gmail.com>
2021-03-21 18:46:05 +0530

    created handleErrors and createToken function in controllers

---
+++ server/controllers/helperFunctions.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken');
+
+const MaxAge = 3 * 24 * 60 * 60;
+
+const handleErrors = (err) => {
+
+    let errors = { email: '', password: '' };
+  
+    if (err.message === 'Incorrect Email') {
+        errors.email = 'That email is not registered yet !';
+    }
+  
+    if (err.message === 'Incorrect Password') {
+        errors.password = 'That password is incorrect';
+    }
+  
+    // duplicate email error
+    if (err.code === 11000) {
+        errors.email = 'This email is already registered.';
+        return errors;
+    }
+  
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({ properties }) => {
+        errors[properties.path] = properties.message;
+      });
+    }
+    
+    return errors;
+}
+
+const createToken = (id) => {
+    return jwt.sign({ id }, 'super secret chat-app in react', {
+        expiresIn: MaxAge
+    });
+}
+
+module.exports = {
+    createToken,
+    handleErrors
+}
