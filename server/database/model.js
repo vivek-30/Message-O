0e1d0ee62e723bc91e95be5a2e1a51c17d8e8ed1
vivek singh <vikky30singh@gmail.com>
2021-03-21 18:23:02 +0530

    added pre hook and static login method

--- server/database/model.js
+++ server/database/model.js
@@ -1,24 +1,49 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-
+    
     name: {
-        type: String,
-        required: true,
+        type: String
     },
     email: {
-        type: String
+        type: String,
+        unique: true,
+        required: [ true, 'Email is required field' ]
     },
     password: {
         type: String,
-        required: true
+        required: [ true, 'Password is required field' ],
+        minlenght: [ 6, 'Password must be atleast 6 characters long' ]
     },
     contact: {
         type: Number
     }
 });
 
-const Model = mongoose.model('user',userSchema);
+// configure pre hook.
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
-module.exports = Model;
+userSchema.statics.login = async function(email, password) {
+    const user = this.findOne({ email });
+    if(user) {
+        // Return true if matches else return false
+        const verified = bcrypt.compare(password, user.password);
+        if(verified) {
+            return user;
+        }else {
+            throw Error('Incorrect Password');
+        }
+    }else {
+        throw Error('Incorrect Email');
+    }
+}
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
