9261023209b93cd0808a96ba16d411026af92517
vivek singh <vikky30singh@gmail.com>
2021-03-22 12:55:17 +0530

    handled post request on sign-in route with saving token on cookie

--- src/components/authentication/SignIn.js
+++ src/components/authentication/SignIn.js
@@ -4,18 +4,38 @@ import './authenticate.css'
 
 const SignIn = () => {
 
-    const [ user, setUser ] = useState('')
+    const [ email, setEmail ] = useState('')
     const [ password, setPassword ] = useState('')
 
     const handleChange = (e) => {
         const { id, value } = e.target
-        id === 'user-name' ? setUser(value) : id ===  'password' ? setPassword(value) : null
+        id === 'user-email' ? setEmail(value) : id ===  'password' ? setPassword(value) : null
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(`user = ${user} password = ${password}`)
-        setUser('')
+        // console.log(`user = ${user} password = ${password}`)
+        fetch('http://127.0.0.1:4000/sign-in', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: "include",
+            body: JSON.stringify({
+                email,
+                password
+            })
+        }).then((res) => {
+            if(res.status === 200 || res.status === 201) {
+                return res.json()
+            }
+        }).then((result) => {
+            console.log('signin result -> ',result)
+        }).catch((err) => {
+            console.error('error in signin', err);
+            alert('Error logging in please try again');
+        })
+        setEmail('')
         setPassword('')
     }
 
@@ -23,13 +43,13 @@ const SignIn = () => {
         <form className="authenticate" onSubmit={handleSubmit}>
             <h4 className="blue-text text-darken-2">Sign In</h4>
             <TakeInput 
-                iconName="person" 
-                labelText="Enter Your Username"
+                iconName="mail" 
+                labelText="Enter Your Email"
                 options={{
-                    id: "user-name", 
-                    type: 'text',
+                    id: "user-email", 
+                    type: 'email',
                     handleChange,
-                    value: user
+                    value: email
                 }}
             />
 
