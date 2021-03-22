e176ec4754e6d21714d72f1aa927556c0e388619
vivek singh <vikky30singh@gmail.com>
2021-03-22 14:44:20 +0530

    created makePostRequest helper function

--- src/client/helperFunctions.js
+++ src/client/helperFunctions.js
@@ -8,3 +8,40 @@ export const customAlert = (message) => {
         displayLength: 2000
     })
 }
+
+export const makePostRequest = (URL, Data) => {
+
+    var output = 'No output yet!'
+
+    fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: "include",
+            body: JSON.stringify(Data)
+    })
+    .then((response) => {
+        if(response.status === 200 || response.status === 201) {
+            // this.props.history.push('/');
+            // console.log(res)
+            return response.json()
+        }
+        else {
+            console.log(response.json(), response)
+            const error = new Error(response.error)
+            throw error;
+        }
+    })
+    .then((data) => {
+        console.log('signup result ->', data)
+        output = data;
+    })
+    .catch((err) => {
+        console.error(err);
+        output = err;
+        alert('Error logging in please try again')
+    })
+
+    return output
+}
