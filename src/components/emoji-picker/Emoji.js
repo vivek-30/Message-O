d66a0bf5378ad8c6020aa7f96b1ad8c0c37822bc
vivek singh <vikky30singh@gmail.com>
2021-03-25 00:31:34 +0530

    extended emoji range in emoji picker

--- src/components/emoji-picker/Emoji.js
+++ src/components/emoji-picker/Emoji.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react'
 import { Picker } from 'emoji-mart';
-import { CustomEmotes } from './CustomEmotes'
 import { displayEmoji } from '../main/Form'
 import './emojiStyle.css'
 
 const pickerStyle = { 
     top: '8rem',
     right: '3rem',
-    width: '15rem',  
+    width: '18rem',  
     position: 'absolute',
-    backgroundColor: '#f1f1f1',
+    backgroundColor: '#eeeeee',
     boxShadow: '2px 2px 8px 1px rgba(0,0,1,0.3)'
 }
 
@@ -28,8 +27,6 @@ const Emoji = () => {
             }}
             emojiSize={30} 
             style={{ display: picker ? 'block' : 'none', ...pickerStyle }}
-            include={['custom']}
-            custom={CustomEmotes} 
             />
         </span>
     )
