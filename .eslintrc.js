0b898e5ddc50d4f058c36a635a52ae3f0b056fc5
vivek singh <vikky30singh@gmail.com>
2021-03-25 01:22:33 +0530

    added few rules in .eslintrc file for code consistency

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,14 @@ module.exports = {
     ],
     "parser": "babel-eslint",
     "rules": {
-      "react/prop-types": "off"
+      "react/prop-types": "off",
+      "semi": [ 1, "never" ],
+      "comma-dangle": [ 1, "never" ],
+      "comma-spacing": [ 1, { "before": false, "after": true } ],
+      "brace-style": [ 1, "stroustrup", { "allowSingleLine": true } ],
+      "object-curly-spacing": [ 1, "always" ],
+      "no-sparse-arrays": 1,
+      "eqeqeq": 1
     },
     "settings": {
       "react": {
