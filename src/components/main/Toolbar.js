c7fd12ce68cb7a6220ffbd678542881721349750
vivek singh <vikky30singh@gmail.com>
2021-03-26 23:37:30 +0530

    added new feature of setting up background wallpaper in chat window

--- src/components/main/Toolbar.js
+++ src/components/main/Toolbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import 'emoji-mart/css/emoji-mart.css'
 import Emoji from '../emoji-picker/Emoji'
 import { useHistory } from 'react-router-dom'
@@ -9,6 +9,17 @@ const Toolbar = () => {
     const [ picker, setPicker ] = useState(false)
 
     const history = useHistory()
+    const fileRef = useRef(null)
+
+    const handleFileInput = () => {
+        var value = fileRef.current.files[0]
+        var url = URL.createObjectURL(value)
+        var chatWindow = document.getElementById('chat-window')
+        chatWindow.style.background = `url(${url}) no-repeat center`
+        chatWindow.style.backgroundSize = 'cover'
+        // chatWindow.style.opacity = '0.7'
+        chatWindow.style.zIndex = -1
+    }
 
     const Logout = () => {
         fetch('http://127.0.0.1:4000/logout', {
@@ -27,7 +38,10 @@ const Toolbar = () => {
         <>
             { picker && (<Emoji />) }
             <div className="grey darken-4" id="features">
-                <span><i className="material-icons small blue-text text-darken-2">wallpaper</i></span>
+                <span onClick={() => { fileRef.current.click() }}>
+                    <input type="file" accept="image/*" ref={fileRef} style={{ display: 'none' }} onChange={handleFileInput} />
+                    <i className="material-icons small blue-text text-darken-2">wallpaper</i>
+                </span>
                 <span><i className="material-icons small blue-text text-darken-2">video_call</i></span>
                 <span onClick={Logout}>
                     <i className="material-icons small blue-text text-darken-2">home</i>
--- src/components/majorComponents/ChatWindow.js
+++ src/components/majorComponents/ChatWindow.js
@@ -42,6 +42,7 @@ const ChatWindow = () => {
             })
             setUser(user)
         })
+        
     }, [])
 
     useEffect(() => {
