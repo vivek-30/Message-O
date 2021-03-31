440fd99d52858d897fc54645d35b5bffc9e8c0c2
vivek singh <vikky30singh@gmail.com>
2021-03-31 13:56:48 +0530

    created dropdown component

---
+++ src/client/dropdown/DropDown.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './dropDown.css'
+
+const DropDown = () => {
+
+    return (
+        <div id="drop-down">
+            <p>clear chat</p>
+            <p>Toggle Mode</p>
+            <p>Reset Background</p>
+        </div>
+    )
+}
+
+export default DropDown
