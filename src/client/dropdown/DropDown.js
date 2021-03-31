72c132fcce0adf1f0623167e7114ff1ddf6253b4
vivek singh <vikky30singh@gmail.com>
2021-03-31 15:36:52 +0530

    implemented reset-bg and clear-chat feature of dropDown component

--- src/client/dropdown/DropDown.js
+++ src/client/dropdown/DropDown.js
@@ -1,13 +1,35 @@
 import React from 'react'
+import { customAlert, removeWallpaper } from '../helperFunctions'
 import './dropDown.css'
 
-const DropDown = () => {
+const DropDown = ({ setData }) => {
+
+    const handleChoice = (e) => {
+        let name = e.target.getAttribute('name')
+        switch(name) {
+            case 'clear-chat': {
+                setData([])
+                break
+            }
+            case 'toggle-theme': {
+                customAlert('This feature is comming soon ....')
+                break
+            }
+            case 'reset-bg': {
+                removeWallpaper()
+                break
+            }
+            default: {
+                console.log('None of the available option')
+            }
+        }
+    }
 
     return (
         <div id="drop-down">
-            <p name="clear-chat">clear chat</p>
-            <p name="toggle-theme">Toggle Mode</p>
-            <p name="reset-bg">Reset Background</p>
+            <p name="clear-chat" onClick={handleChoice}>clear chat</p>
+            <p name="toggle-theme" onClick={handleChoice}>Toggle Mode</p>
+            <p name="reset-bg" onClick={handleChoice}>Reset Background</p>
         </div>
     )
 }
