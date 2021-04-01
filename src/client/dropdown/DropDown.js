c539c7dd0de10f3ac4fbce21aa0d482e8a93eb3a
vivek singh <vikky30singh@gmail.com>
2021-04-01 12:51:23 +0530

    implemented dark-mode function and styled drop-down component

--- src/client/dropdown/DropDown.js
+++ src/client/dropdown/DropDown.js
@@ -1,8 +1,24 @@
 import React from 'react'
-import { customAlert, removeWallpaper } from '../helperFunctions'
+import { removeWallpaper } from '../helperFunctions'
+import toggleTheme from '../darkMode/toggleTheme'
 import './dropDown.css'
 
-const DropDown = ({ setData }) => {
+const lightDropDown = {
+    border: '1px solid #f3f8f9',
+    backgroundColor: '#f2f2f2',
+    boxShadow: '2px 2px 2px #ccc'
+}
+
+const darkDropDown = {
+    border: '1px solid #46b5d1',
+    backgroundColor: '#424242',
+    color: '#ccc',
+    boxShadow: '2px 2px 2px #222222'
+}
+
+const DropDown = ({ theme, setOptions, setData, setTheme }) => {
+
+    const currentTheme = theme === 'light' ? lightDropDown : darkDropDown
 
     const handleChoice = (e) => {
         let name = e.target.getAttribute('name')
@@ -12,21 +28,23 @@ const DropDown = ({ setData }) => {
                 break
             }
             case 'toggle-theme': {
-                customAlert('This feature is comming soon ....')
+                toggleTheme()
+                setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light')
                 break
             }
             case 'reset-bg': {
-                removeWallpaper()
+                removeWallpaper(theme)
                 break
             }
             default: {
                 console.log('None of the available option')
             }
         }
+        setOptions(prevOptions => !prevOptions)
     }
 
     return (
-        <div id="drop-down">
+        <div id="drop-down" style={{ ...currentTheme }}>
             <p name="clear-chat" onClick={handleChoice}>clear chat</p>
             <p name="toggle-theme" onClick={handleChoice}>Toggle Mode</p>
             <p name="reset-bg" onClick={handleChoice}>Reset Background</p>
