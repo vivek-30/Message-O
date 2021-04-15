e700185e67f5466cddc1d13523add6538ed96ed7
vivek singh <vikky30singh@gmail.com>
2021-04-15 16:07:58 +0530

    added modal in UI and handled it appearence

--- src/components/main/Toolbar.js
+++ src/components/main/Toolbar.js
@@ -2,11 +2,14 @@ import React, { useState, useRef } from 'react'
 import 'emoji-mart/css/emoji-mart.css'
 import Emoji from '../emoji-picker/Emoji'
 import { useHistory } from 'react-router-dom'
+import Modal from './Modal'
+import { socket } from '../../client/Chat'
 import { customAlert, handleFileInput } from '../../client/helperFunctions'
 
 const Toolbar = ({ setOptions, theme }) => {
 
-    const [ picker, setPicker ] = useState(false)
+    const [ picker, setPicker ] = useState(false)    
+    const [ instance, setInstance ] = useState(false)
     
     const history = useHistory()
     const fileRef = useRef(null)
@@ -30,7 +33,7 @@ const Toolbar = ({ setOptions, theme }) => {
     return (
         <>
             { picker && <Emoji theme={theme} /> }
-            <div className={`grey darken-4 ${theme === 'dark' ? 'dark-features' : ''}`} id="features">
+            <div className={`grey darken-4 ${theme === 'dark' ? 'dark-modal' : ''}`} id="features">
                 <span name="bg-wallpaper" onClick={() => { fileRef.current.click() }}>
                     <input 
                         type="file" 
@@ -41,7 +44,10 @@ const Toolbar = ({ setOptions, theme }) => {
                     />
                     <i className={`material-icons small ${iconStyle}`}>wallpaper</i>
                 </span>
-                <span name="video-call" onClick={() => { history.push('/VideoCall') }}>
+                <span name="video-call" onClick={() => {
+                    socket.emit('get-users-list')
+                    instance ? instance?.open() : customAlert('Internal Error in Loding up the Modal') 
+                }}>
                     <i className={`material-icons small ${iconStyle}`}>video_call</i>
                 </span>
                 <span name="logout" onClick={Logout}>
@@ -54,6 +60,9 @@ const Toolbar = ({ setOptions, theme }) => {
                     <i className={`material-icons small ${iconStyle}`}>more_horiz</i>
                 </span>
             </div>
+            <Modal theme={theme} getInstance={(instance) => {
+                instance ? setInstance(instance) : console.log('Can Not Get Modal')
+            }} />
         </>
     )
 }
