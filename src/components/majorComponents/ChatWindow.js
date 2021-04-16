c58c66c06b420520fd2ec53eb3b7acb5a8b1a457
vivek singh <vikky30singh@gmail.com>
2021-04-16 18:18:33 +0530

    added a new component notifier and handled some click functionality

--- src/components/majorComponents/ChatWindow.js
+++ src/components/majorComponents/ChatWindow.js
@@ -1,20 +1,27 @@
 import React, { useState, useEffect, useRef } from 'react'
+import { useHistory } from 'react-router-dom'
 import { socket } from '../../client/Chat'
 import Message from '../../client/Message'
 import chatTone from '../../../public/chat_tone.mp3'
 import { setWallpaper } from '../../client/helperFunctions'
 import DropDown from '../../client/dropdown/DropDown'
+import { callRejected, answerCall } from './videoCall/VideoCall'
 import ToolBar from '../main/Toolbar'
 
 const ChatWindow = ({ theme, setTheme }) => {
 
     const [ data, setData ] = useState([])
+    const [ callingUserID, setCallingUserID ] = useState(null)
     const [ user, setUser ] = useState('')
+    const [ callingUser, setCallingUser ] = useState('')
     const [ options, setOptions ] = useState(false)
+    const [ displayNotifier, setDisplayNotifier ] = useState(false)
     const [ CSS, setCSS ] = useState({
         backgroundColor: '#c2c2c2', 
         padding: '8px 20px'
     })
+
+    const history = useHistory()
 
     const bottomScrollRef = useRef(null)
     const audioRef = useRef(null)
@@ -31,6 +38,18 @@ const ChatWindow = ({ theme, setTheme }) => {
                 newData['user'] = 'you'
                 setData(data => [ ...data, newData ])
             })
+
+            socket.on('notify-user', ({ user, myID }) => {
+                setDisplayNotifier(true)
+                setCallingUserID(myID)
+                setCallingUser(user)
+                setTimeout(() => { 
+                    // setDisplayNotifier(false)
+                    callRejected()
+                }, 10000)
+            })
+
+            socket.on('stop-notifying', () => console.log('stop notifying'))
 
             socket.on('chat', (newData) => {
                 setCSS({
@@ -67,6 +86,20 @@ const ChatWindow = ({ theme, setTheme }) => {
 
     return (
         <>
+            { displayNotifier && (
+                <div id="notifier" className={`${theme === 'dark' ? 'dark-notifier dark-border' : 'light-notifier light-shadow'}`}>
+                    <div id="notifier-title" className={`center ${theme === 'dark' ? 'grey-text' : 'black-text'}`}>
+                        {callingUser} wants you to join video conference
+                    </div>
+                    <div className={`${theme === 'dark' ? 'dark-notifier' : 'light-notifier'}`}>
+                        <button className="left btn-flat blue-text" onClick={callRejected}>Cancel</button>
+                        <button className="right btn-flat blue-text" onClick={() => {
+                            history.push(`VideoCall/${callingUserID}`)
+                            setTimeout(() => answerCall(), 3000)
+                        }}>Join</button>
+                    </div>
+                </div>
+            ) }
             <div style={{ backgroundColor: '#f2f2f2', marginTop: '0.4rem' }} id="outer-chat-box">
                 <div id="chat-window" className={theme === 'dark' ? 'dark-window' : ''}>
                     <div id="display">
