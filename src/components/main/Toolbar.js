df772465e3a79e26b2784fea0cf6470371c058ef
vivek singh <vikky30singh@gmail.com>
2021-04-21 22:59:19 +0530

    removed tooltipp(s)

--- src/components/main/Toolbar.js
+++ src/components/main/Toolbar.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import 'emoji-mart/css/emoji-mart.css'
-import M from 'materialize-css'
 import Emoji from '../emoji-picker/Emoji'
 import { useHistory } from 'react-router-dom'
 import Modal from './Modal'
@@ -15,11 +14,6 @@ const Toolbar = ({ setOptions, theme }) => {
     const history = useHistory()
     const fileRef = useRef(null)
 
-    useEffect(() => {
-        var elems = document.querySelectorAll('.tooltipped')
-        elems ? M.Tooltip.init(elems, { position: 'top' }) : null
-    }, [])
-
     const Logout = () => {
         fetch('http://127.0.0.1:4000/logout', {
             method: 'GET',
@@ -27,7 +21,7 @@ const Toolbar = ({ setOptions, theme }) => {
         })
         .then((response) => {
             response.ok ? history.push('/SignIn') 
-            : customAlert('There is a problem in server please try again later', theme)
+            : customAlert('There is a problem in server please try after some time', theme)
         })
         .catch((err) => {
             console.log('Error in Logging out', err)
@@ -40,7 +34,7 @@ const Toolbar = ({ setOptions, theme }) => {
         <>
             { picker && <Emoji theme={theme} /> }
             <div className={`grey darken-4 ${theme === 'dark' ? 'dark-modal' : ''}`} id="features">
-                <span className="tooltipped" data-tooltip="Set Background Wallpaper" name="bg-wallpaper" onClick={() => { fileRef.current.click() }}>
+                <span name="bg-wallpaper" onClick={() => { fileRef.current.click() }}>
                     <input 
                         type="file" 
                         accept="image/*" 
@@ -50,19 +44,19 @@ const Toolbar = ({ setOptions, theme }) => {
                     />
                     <i className={`material-icons small ${iconStyle}`}>wallpaper</i>
                 </span>
-                <span className="tooltipped" data-tooltip="Make Video Call" name="video-call" onClick={() => {
+                <span name="video-call" onClick={() => {
                     socket.emit('get-users-list')
                     instance ? instance?.open() : customAlert('Internal Error in Loding up the Modal', theme) 
                 }}>
                     <i className={`material-icons small ${iconStyle}`}>video_call</i>
                 </span>
-                <span className="tooltipped" data-tooltip="Logout" name="logout" onClick={Logout}>
+                <span name="logout" onClick={Logout}>
                     <i className={`material-icons small ${iconStyle}`}>home</i>
                 </span>
-                <span className="tooltipped" data-tooltip="Emoji Picker" name="emoji-picker" onClick={() => setPicker(!picker)}>
+                <span name="emoji-picker" onClick={() => setPicker(!picker)}>
                     <i className={`material-icons small ${iconStyle}`}>insert_emoticon</i>
                 </span>
-                <span className="tooltipped" data-tooltip="More ..." name="more" onClick={() => setOptions(prevOptions => !prevOptions)}>
+                <span name="more" onClick={() => setOptions(prevOptions => !prevOptions)}>
                     <i className={`material-icons small ${iconStyle}`}>more_horiz</i>
                 </span>
             </div>
